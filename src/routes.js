7b74e76a6ed74d2d913e369c26b794d09030a66e
nubelsondev <nubelsondev@hotmail.com>
2019-12-28 21:32:06 +0000

    feat: set up routes file

---
+++ src/routes.js
@@ -0,0 +1,7 @@
+const routes = require('express').Router()
+
+routes.get('/', (req, res) => {
+	res.json({ hello: 'World!' })
+})
+
+module.exports = routes
--- src/server.js
+++ src/server.js
@@ -1,10 +1,9 @@
 const express = require('express')
+const routes = require('./routes')
 
 const app = express()
 const port = process.env.PORT || 3333
 
-app.get('/', (req, res) => {
-	res.json({ hello: 'World!' })
-})
+app.use(routes)
 
 app.listen(port, () => console.log(`Port ${port} is open.`))
