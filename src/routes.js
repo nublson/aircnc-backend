f668cd064ea550f5b0dce12acbad1ca9fe4c116d
nubelsondev <nubelsondev@hotmail.com>
2019-12-29 03:44:28 +0000

    feat: add controller to list spots by tech

--- src/routes.js
+++ src/routes.js
@@ -9,6 +9,7 @@ const upload = multer(uploadConfig)
 
 routes.post('/sessions', SessionController.store)
 
+routes.get('/spots', SpotController.index)
 routes.post('/spots', upload.single('thumbnail'), SpotController.store)
 
 module.exports = routes
