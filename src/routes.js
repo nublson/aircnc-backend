359fa33e422fe960b4f98c27effcd57ee7d10d27
nubelsondev <nubelsondev@hotmail.com>
2019-12-29 01:40:02 +0000

    feat: add Session controller with store method

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 const routes = require('express').Router()
 
-routes.get('/', (req, res) => {
-	res.json({ hello: 'World!' })
-})
+const SessionController = require('./controllers/SessionController')
+
+routes.post('/sessions', SessionController.store)
 
 module.exports = routes
