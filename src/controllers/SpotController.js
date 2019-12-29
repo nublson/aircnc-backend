4f54e83aa72f3b79e148b3f277a00bfb9685a21b
nubelsondev <nubelsondev@hotmail.com>
2019-12-29 03:25:24 +0000

    fix: set up store method to create Spots

--- src/controllers/SpotController.js
+++ src/controllers/SpotController.js
@@ -3,6 +3,28 @@ const Spot = require('../models/Spot')
 
 module.exports = {
 	async store(req, res) {
-		res.json({ ok: true })
+		const { filename: thumbnail } = req.file
+		const { company, price, techs } = req.body
+
+		// Access the user id from the Header
+		const { user_id } = req.headers
+
+		// Verify user
+		const user = await User.findById(user_id)
+
+		if (!user) {
+			return res.status(400).json({ error: 'User does not exists!' })
+		}
+
+		// Create spot
+		const spot = await Spot.create({
+			thumbnail,
+			company,
+			price,
+			techs: techs.split(',').map(tech => tech.trim().toUpperCase()),
+			user: user_id
+		})
+
+		res.json(spot)
 	}
 }
--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,14 @@
-const routes = require('express').Router()
+const multer = require('multer')
 
+const uploadConfig = require('./config/upload')
 const SessionController = require('./controllers/SessionController')
 const SpotController = require('./controllers/SpotController')
 
+const routes = require('express').Router()
+const upload = multer(uploadConfig)
+
 routes.post('/sessions', SessionController.store)
 
-routes.post('/spots', SpotController.store)
+routes.post('/spots', upload.single('thumbnail'), SpotController.store)
 
 module.exports = routes
